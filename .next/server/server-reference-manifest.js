36d7ad52257a73fff70f7ddeb0ef39aac097d18e
Misel Tekinder <misel.tekinder@gmail.com>
2025-02-14 21:49:43 +0100

    App Build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"EJ7+9prhkAz4ixdFVOnOIeYZ5VXDdrY0dFC4ju273oo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
